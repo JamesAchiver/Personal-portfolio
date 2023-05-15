101b0e986a73746eba9fb98e1b062fcc348fabe2
Yash James <deutero2021@gmail.com>
2023-05-15 14:44:28 +0530

    -- Version 1.4.2 -- Updated + Correction

---
+++ Practise/Dashboard.js
@@ -0,0 +1,17 @@
+// Dashboard JavaScript File ::
+
+
+const text = `Hello  , ` +  ` I'm Yash James. `; // The text to be typed
+const speed = 80; // Speed of typing (in milliseconds)
+const typedTextElement = document.getElementById("typed-text");
+let charIndex = 0;
+
+function type() {
+  if (charIndex < text.length) {
+    typedTextElement.textContent += text.charAt(charIndex);
+    charIndex++;
+    setTimeout(type, speed);
+  }
+}
+
+type();
