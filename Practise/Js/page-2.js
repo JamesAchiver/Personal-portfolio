7b4df06896e44150feb37429103539735dbebeb5
Yash James <deutero2021@gmail.com>
2023-05-21 21:59:06 +0530

    -- Version -- 1.4.4 -- Proper Working

--- Practise/Js/page-2.js
+++ Practise/Js/page-2.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", function() {
+     console.log("Website loaded");
      const loaderOverlay = document.getElementById("loader-overlay");
      loaderOverlay.style.display = "none";
 });
+
+
    
--- Practise/Js/page-3.js
+++ Practise/Js/page-3.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function() {
+     console.log("Website loaded");
      const loaderOverlay = document.getElementById("loader-overlay");
      loaderOverlay.style.display = "none";
 });
