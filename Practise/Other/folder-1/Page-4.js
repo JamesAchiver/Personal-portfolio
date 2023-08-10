c85fafe2119bb65f723b5841d9274b09c4309015
Yash James <deutero2021@gmail.com>
2023-08-10 15:22:21 +0530

    Project -1 Added Tentative

---
+++ Practise/Other/folder-1/Page-4.js
@@ -0,0 +1,8 @@
+// code for loading animation in HTML Page 
+
+window.addEventListener("load", function() {
+     console.log("Website loaded");
+     const loaderOverlay = document.getElementById("loader-overlay");
+     loaderOverlay.style.display = "none";
+});
+   
