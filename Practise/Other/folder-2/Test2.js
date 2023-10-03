cd0ef10f340a2de2563ebe10b724121b275d1706
Yash James <deutero2021@gmail.com>
2023-10-03 23:57:20 +0530

    Still Working

---
+++ Practise/Other/folder-2/Test2.js
@@ -0,0 +1,23 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+     // Get the overlay and close button elements
+     const overlay = document.getElementById('overlay');
+     const closeOverlayButton = document.getElementById('closeOverlayButton');
+ 
+     // Function to show the overlay
+     function showOverlay() {
+         overlay.style.display = 'flex';
+     }
+ 
+     // Function to close the overlay
+     function closeOverlay() {
+         overlay.style.display = 'none';
+     }
+ 
+     // Show the overlay when the page loads
+     showOverlay();
+ 
+     // Event listener to close the overlay when the close button is clicked
+     closeOverlayButton.addEventListener('click', closeOverlay);
+});
+ 
